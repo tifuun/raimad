1ffe4eb3737ef3de7cd2959941a1406b69a5202f
maybetree <maybetree48@proton.me>
2023-11-03 08:37:02 +0100

    Revert "Migrate to pyproject.toml"
    This reverts commit 054031d188006d41a9b47ffa06734e61660f72b3.

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+setup.py -- setuptools metadata.
+
+This file is needed to tell setuptools (an underlying compoent of pip)
+about PyCIF.
+"""
+
+__version__ = '0.0.0'
+__author__ = 'Nikita Soshnin'
+__email__ = 'N.A.Soshnin@student.tudelft.nl'
+__license__ = "I haven't decided yet"
+
+import pathlib
+import setuptools
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setuptools.setup(
+    name='PyCIF',
+    version=__version__,
+    description=(
+        'Collaborative CAD platform for hierarchical '
+        'design of on-chip imaging devices'
+        ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author=__author__,
+    author_email=__email__,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        ],
+    packages=setuptools.find_packages(),
+    python_requires='>=3',
+    install_requires=[
+        'numpy==1.24.2',
+        'addict==2.4.0',
+        ],
+    extras_require={
+        'modulebrowser': [
+            'jinja2==3.1.2',
+            ],
+        },
+)
+
