7480d90ec6b80a079bcaec530247e9fd04c2ef1b
maybetree <maybetree48@proton.me>
2024-03-30 08:10:28 +0100

    Lowercase filenames

--- src/pycif/__init__.py
+++ src/pycif/__init__.py
@@ -6,19 +6,19 @@ radians = lambda r: r
 
 from pycif.helpers import *
 
-from pycif.Mark import Mark, MarkAnnot
-from pycif.Layer import Layer, LayerAnnot
-from pycif.Option import Option, OptionAnnot
-from pycif.Transform import Transform
-from pycif.Compo import Compo
-from pycif.Proxy import Proxy
-from pycif.BoundPoint import BoundPoint
-from pycif.BBox import BBox
+from pycif.mark import Mark, MarkAnnot
+from pycif.layer import Layer, LayerAnnot
+from pycif.option import Option, OptionAnnot
+from pycif.transform import Transform
+from pycif.compo import Compo
+from pycif.proxy import Proxy
+from pycif.boundpoint import BoundPoint
+from pycif.bbox import BBox
 
-from pycif.RectWH import RectWH
-from pycif.RectWire import RectWire
-from pycif.Circle import Circle
-from pycif.AnSec import AnSec
+from pycif.rectwh import RectWH
+from pycif.rectwire import RectWire
+from pycif.circle import Circle
+from pycif.ansec import AnSec
 
 from pycif.cif import export_cif
 from pycif import err
--- src/pycif/err.py
+++ src/pycif/err.py
@@ -1,12 +1,12 @@
-from pycif.AnSec import AnSecError
-from pycif.AnSec import AnSecRadiusError
-from pycif.AnSec import AnSecRadiusTooManyArgumentsError
-from pycif.AnSec import AnSecRadiusNotEnoughArgumentsError
-from pycif.AnSec import AnSecRadiusIncorrectArgumentsError
-from pycif.AnSec import AnSecThetaError
-from pycif.AnSec import AnSecThetaTooManyArgumentsError
-from pycif.AnSec import AnSecThetaNotEnoughArgumentsError
-from pycif.AnSec import AnSecThetaIncorrectArgumentsError
+from pycif.ansec import AnSecError
+from pycif.ansec import AnSecRadiusError
+from pycif.ansec import AnSecRadiusTooManyArgumentsError
+from pycif.ansec import AnSecRadiusNotEnoughArgumentsError
+from pycif.ansec import AnSecRadiusIncorrectArgumentsError
+from pycif.ansec import AnSecThetaError
+from pycif.ansec import AnSecThetaTooManyArgumentsError
+from pycif.ansec import AnSecThetaNotEnoughArgumentsError
+from pycif.ansec import AnSecThetaIncorrectArgumentsError
 
-from pycif.BBox import EmptyBBoxError
+from pycif.bbox import EmptyBBoxError
 
