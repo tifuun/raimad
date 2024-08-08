7e0a050c7a3f311162a3c69be5947d2a2719ffaf
maybetree <maybetree48@proton.me>
2024-08-08 15:46:32 +0200

    Hooray! Mypy has been appeased!

--- src/raimad/custompoly.py
+++ src/raimad/custompoly.py
@@ -33,32 +33,15 @@ class CustomPoly(rai.Compo):
                 tuple[str, tuple[float, float]]
                 ]
             ) -> None:
-        processed_points = []
+        processed_points: 'list[rai.typing.Point]' = []
         for point in points:
-            match point:
-                case (
-                        str() as mark_name,
-                        (float() | int(), float() | int()) as point
-                        ):
-                    self.marks[mark_name] = point
 
-                case (float() | int(), float() | int()) as point:
-                    # Very confusing,
-                    # when it comes to type annotations,
-                    # `int` is treated as a subset of `float`,
-                    # but in structural pattern matching,
-                    # an `int` is not a `float`.
-                    pass
+            if isinstance(point[0], str):
+                self.marks[point[0]] = point[1]
+                processed_points.append(point[1])
 
-                case _:
-                    raise CustomPolyException(
-                        "I have no clue what you want me to do with "
-                        f"`{point}`"
-                        )
-            processed_points.append(point)
-            # TODO would be cool to make it possible to
-            # specify coordinates relative to the previous
-            # point as opposed to absolute.
+            else:
+                processed_points.append(point)
 
         self.geoms.update({'root': [processed_points]})
 
