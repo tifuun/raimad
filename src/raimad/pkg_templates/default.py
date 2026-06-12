8dc262a75e56dfafea6380e26c71564ecad1bf4c
maybetree <maybetree48@proton.me>
2026-06-12 14:09:31 +0200

    add tests to template

--- src/raimad/pkg_templates/default.py
+++ src/raimad/pkg_templates/default.py
@@ -909,9 +909,45 @@ CMakeInit.txt
 import raimad as rai
 
 class $COMPO_CAMEL(rai.Compo):
-    def _make(self):
-        raise NotImplementedError()
+    class Options:
+        width = rai.Option.Geometric("Width of the wire")
+
+    class Layers:
+        nbtin_opt = rai.Layer("Niobium-titanium-nitride optical deposition")
+
+    class Marks:
+        tip = rai.Mark("tip of the wire")
+
+    browser_tags = ["example"]
+    _experimental_extra_lname_transformers = [{
+        'nbtin_opt': 'NBTN',
+        }]
+
+    _experimental_lyp = {
+        'NBTN': rai.lyp.Properties(
+            fill_color='#555555',
+            frame_color='#880088',
+            ),
+        }
+
+    def _make(self, width: float = 10):
+        self.subcompos.box = rai.RectLW(20, width).proxy().map('nbtin_opt')
+        self.marks.tip = self.subcompos.box.bbox.mid_right
 """),
         },
+    },
+    "tests": {
+        Template("test_$COMPO_SNAKE.py"): Template("""\
+import unittest
+
+import raimad as rai
+import $NAME
+
+class Test$COMPO_CAMEL(unittest.TestCase):
+    def test_$COMPO_SNAKE(self):
+        compo = $NAME.$COMPO_SNAKE(width=12)
+        self.assertEqual(compo.bbox.width, 12)
+
+"""),
     },
 }
