ea9c8699733defb048f7b5763fea428a0bc3baf5
maybetree <maybetree48@proton.me>
2026-06-16 13:51:16 +0200

    cleanup template test

--- tests/test_template.py
+++ tests/test_template.py
@@ -8,75 +8,75 @@ import subprocess
 import unittest
 import tempfile
 from pathlib import Path
+from typing import Sequence, cast
 
 import raimad as rai
 
-class TestTemplate(unittest.TestCase):
-    #def __init__(self, *args, **kwargs):
-    #    super().__init__(*args, **kwargs)
+def spawn_python(
+        args: Sequence[str],
+        cwd: str | Path,
+        pythonpath: Sequence[str] | None = None,
+        ) -> subprocess.Popen[str]:
+        # the [str] generic means the Popen is in text mode
+        # (`text=True`)
 
-    #    self.snowman_cif = rai.export_cif(rai.Snowman())
-    #    self.rectlw_cif = rai.export_cif(rai.RectLW(420, 6.9))
-    #    self.rectlw_defaults_cif = rai.export_cif(rai.RectLW(
-    #        length=rai.RectLW.Options.length.browser_default,
-    #        width=rai.RectLW.Options.width.browser_default,
-    #        ))
-    #    self.rectlw_defaults_override_cif = rai.export_cif(rai.RectLW(
-    #        length=rai.RectLW.Options.length.browser_default,
-    #        width=6.9,
-    #        ))
+    env = {}
+
+    if pythonpath is not None:
+        env['PYTHONPATH'] = ':'.join((
+            *os.environ.get('PYTHONPATH', ()),
+            *pythonpath
+        ))
+
+    return subprocess.Popen(
+        (sys.executable, *args),
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        universal_newlines=True,  # TODO needed?
+        cwd=cwd,
+        env=env,
+    )
+
+def send_lines(
+        p: subprocess.Popen[str],
+        lines: Sequence[str]
+        ) -> tuple[str, str]:
+    # extra empty string at the end so there's a trailing newline
+    stdout, stderr = p.communicate('\n'.join((*lines, '')))
+    return stdout, stderr
+
+class TestTemplate(unittest.TestCase):
 
     def test_template_happy_path(self):
-        with tempfile.TemporaryDirectory() as folder:
+        with tempfile.TemporaryDirectory() as tmpfolder:
+            folder = Path(tmpfolder)
 
             # Part one: run the interactive wizard
 
-            p = subprocess.Popen(
-                (sys.executable, '-m', 'raimad', 'init'),
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True,
-                cwd=folder,
-            )
-
-            stdout, stderr = p.communicate("""\
-rai_testpkg
-./testpkg
-This is a test package
-Foo Barr
-foo@barr.com
-FooBarrCompo
-
-""")
-
+            p = spawn_python(('-m', 'raimad', 'init'), folder)
+            send_lines(p, (
+                "rai_testpkg",             # package name
+                "./testpkg",               # path
+                "This is a test package",  # description
+                "Foo Barr",                # author
+                "foo@barr.com",            # email
+                "FooBarrCompo",            # camel
+                "",                        # snake (use default)
+            ))
             self.assertEqual(p.returncode, 0)
 
             # Part two: run autogenned package's unittest
 
-            p = subprocess.Popen(
-                (sys.executable, '-m', 'unittest'),
-                cwd=Path(folder) / 'testpkg',
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True,
-                env={
-                    'PYTHONPATH':
-                    ':'.join((
-                        *os.environ.get('PYTHONPATH', ()),
-                        './src'
-                    ))
-                }
+            p = spawn_python(
+                ('-m', 'unittest'),
+                folder / 'testpkg',  # <- cd into package root
+                pythonpath = ('./src', )   # <- make package `import`able
             )
-
             stdout, stderr = p.communicate()
-
-            self.assertTrue('OK' in stderr)
             self.assertEqual(p.returncode, 0)
-
-            #p = BasicExpect.python_cli('-m unittest')
-            # TODO
+            self.assertTrue('OK' in stderr)
 
 
 if __name__ == '__main__':
